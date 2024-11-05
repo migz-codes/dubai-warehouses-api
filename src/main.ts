dd470449294b79cb3adab3c51e6bb62dbbbfb535
migz-codes <migz.codes@gmail.com>
2024-11-05 09:26:33 -0300

    feat: test with express adapter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ExpressAdapter } from '@nestjs/platform-express'
+import express from 'express'
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  const server = express()
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server))
   app.setGlobalPrefix('api')
-  await app.init();
+  await app.init()
+  return server
 }
 
-bootstrap();
+bootstrap().then(server => {
+  server.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
+  })
+})
